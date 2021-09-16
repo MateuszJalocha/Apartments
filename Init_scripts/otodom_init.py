7bc87c70e8726193aec8a17b14122f1e09f5d151
Mateusz Jałocha <38622419+MateuszJalocha@users.noreply.github.com>
2021-09-16 17:22:13 +0200

    Added numpy

--- Init_scripts/otodom_init.py
+++ Init_scripts/otodom_init.py
@@ -21,6 +21,7 @@ import configparser
 import urllib
 from sqlalchemy import create_engine
 from typing import Tuple, List, Callable, DefaultDict, Union
+import numpy as np
 
 #Sleep while scraping to to avoid blocking
 def scrape_with_sleeping(otodom_scraper: Callable, otodom_links: List[str], time_split: int, time_sleep: int, split_size: int, offers: bool) -> Union[List[str], pd.DataFrame]:
